e7966a1ff7621ea474b86e70f550358f40c0dd35
VHALL <70254697+vhalllive@users.noreply.github.com>
2022-04-08 22:18:23 +0530

    Ui Changes

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,29 +1,68 @@
 import { Constants } from "../../components/utils/Constants";
 import React from "react";
-import axios from "axios";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Heading,
+  Box,
+  Center,
+  Container
+} from '@chakra-ui/react';
 
-export default function Events() {
-  const [data, setData] = React.useState();
 
-  React.useEffect(() => {
-    axios
-      .get(`${Constants.API}${Constants.EVENTS}${Constants.QUERY_POPULATE}`)
-      .then((res) => {
-        console.log(res.data.data);
-        setData(res.data.data);
-      })
-      .catch((error) => console.error(error));
-  }, []);
-
+export default function Events({ events }) {
+  console.log(events);
   return (
     <div>
-      {data &&
-        data.map((item) => (
-          <div key={item.id}>
-            <h1>{item.attributes.Date}</h1>
-          </div>
-        ))}
+      <Box w='100%' p={4}>
+        <Center>
+          <Heading as='h1' size='2xl' >
+            List of Events
+          </Heading>
+        </Center>
+        
+
+      <TableContainer maxW="75%" >
+        <Table variant='striped'>
+          <Thead>
+            <Tr>
+              <Th>Studio</Th>
+              <Th>Date</Th>
+              <Th>EventType</Th>
+              <Th>Slug</Th>
+              <Th>Time</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {events.map((event) => (
+             <Tr key={event.id}>
+                <Td>{event.attributes.Studio}</Td>
+                <Td>{event.attributes.Date}</Td>
+                <Td>{event.attributes.EventType}</Td>
+                <Td>{event.attributes.Slug}</Td>
+                <Td>{event.attributes.Time}</Td>
+             </Tr>   
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      </Box>
     </div>
   )
 }
 
+export async function getStaticProps() {
+  const response = await fetch(`${Constants.API}${Constants.EVENTS}${Constants.QUERY_POPULATE}`);
+  console.log(`${Constants.API}${Constants.EVENTS}${Constants.QUERY_POPULATE}`);
+  const data  = await response.json();
+  return {
+      props: {
+          events : data.data,
+      },
+  }
+}
